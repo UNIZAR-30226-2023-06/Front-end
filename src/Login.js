ce53e9d4c63a3f851931d3ad22487c774063c0e3
Pablo Lacueva <71332626+SrKa0s@users.noreply.github.com>
2023-03-21 16:38:17 +0100

    Revert "cambios deshechos"
    This reverts commit 5fba009db8045f50cae67af680e59bccc8961088.

--- src/Login.js
+++ src/Login.js
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 
 export default function Login() {
   const navigate = useNavigate();
-  var errMessage;
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -21,19 +21,17 @@ export default function Login() {
         },
         body: new URLSearchParams(data),
       })
-        .then((res) => res.json())
-        .then(data =>{
-          if (data.detail === "Incorrect password") {
-            errMessage = "Contraseña incorrecta";
-            reject(errMessage);
-          } else if (data.detail === "Incorrect email") {
-            errMessage = "Correo incorrecto";
-            reject(errMessage);
-          } else if (data.detail === "Logged in successfully") {
-            resolve("Logged in successfully");
+        .then((res) => {
+          console.log(res.json());
+          if (res.ok && res.status === 200) {
+            resolve("Login successfully");
           }
-        }
-    )});
+          reject("Fallo en la recuperacion de la contraseña");
+        })
+        .catch((err) => {
+          reject("Fallo en la recuperacion de la contraseña");
+        });
+    });
   };
 
   return (
@@ -54,7 +52,7 @@ export default function Login() {
             .promise(handleSubmit(e), {
               loading: "Enviando correo...",
               success: "Correo enviado correctamente",
-              error: errMessage,
+              error: "Fallo en la recuperacion de la contraseña",
             })
             .then(() => navigate("/home"));
         }}
