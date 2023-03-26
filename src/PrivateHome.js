7f8515b28d376d87e702fd23f955b623219011d9
Ayelen <ayelennuno@gmail.com>
2023-03-26 01:10:16 +0100

    home retoques

--- src/PrivateHome.js
+++ src/PrivateHome.js
@@ -1,14 +1,13 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 import { useState } from "react";
-
 export default function PrivateHome() {
   const navigate = useNavigate();
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(true);
 
   return (
     // fondo
-    <div className="flex bg-teal-200">
+    <div className="w-full h-full flex bg-teal-200">
       {/* sidebar */}
       <div
         className={`${
@@ -25,39 +24,39 @@ export default function PrivateHome() {
           onClick={() => setOpen(!open)}
         />
         {/* foto del avatar */}
-        <a
-          href="http://localhost:3000/home"
-          class="relative block gap-x-4 items-center"
-        >
+        <div className="relative block gap-x-4 mx-auto">
           <img
             alt="profil"
             src="http://localhost:3000/perfil1.avif"
             className={`mx-auto object-cover rounded-full h-28 w-28 mt-9 ${
               !open &&
-              "h-8 w-8 mx-auto object-cover rounded-full  mt-9 duration-300"
+              "h-10 w-10 mx-auto object-cover mt-9 rounded-full duration-300 justify-center align-middle"
             }`}
           />
+          {/* nombre del usuario */}
           <h1
             className={`text-white origin-center content-center font-medium text-xl duration-300 ${
               !open && `scale-0`
             }`}
+            style={{ display: "flex", justifyContent: "center" }}
           >
-            Name#234567
+            name#234587
           </h1>
-        </a>
-        <ul>
-          <a href="http://localhost:3000/amigos"
-          className="flex items-center"
+        </div>
+        <ul className="flex flex-col w-full items-center py-6 px-4 gap-2 ">
+          <a
+            href="http://localhost:3000/amigos"
+            className="gap-3 mt-2 flex flex-grow"
           >
-            {/* imagen editar perfil*/}
+            {/* imagen amigos*/}
             <img
               alt="profil"
               src="http://localhost:3000/editProfile.png"
-              className={`mx-auto object-cover h-8 w-8 mt-9 ${
-                !open &&
-                "h-8 w-8  object-cover mt-9 duration-300"
+              className={`object-cover h-8 w-8 ${
+                !open && "h-8 w-8 duration-300 ml-8"
               }`}
-            ></img>
+            />
+
             <h1
               href="/login"
               variant={Link}
@@ -65,19 +64,60 @@ export default function PrivateHome() {
                 !open && `scale-0`
               }`}
             >
-              Editar Perfil
+              Editar perfil
             </h1>
           </a>
-          <button
-            type="submit"
-            className="w-full py-2 mt-2 bg-cyan-900 text-white font-bold rounded-full hover:bg-indigo-900 duration-300"
+          <a
+            href="http://localhost:3000/amigos"
+            className="gap-3 mt-2 flex flex-grow"
           >
-            Amigos
-          </button>
+            {/* imagen amigos*/}
+            <img
+              alt="profil"
+              src="http://localhost:3000/friends.png"
+              className={`object-cover h-8 w-8 ${
+                !open && "h-8 w-8 duration-300 ml-12"
+              }`}
+            />
+
+            <h1
+              href="/login"
+              variant={Link}
+              className={`text-white origin-center content-center font-medium text-xl duration-300 ${
+                !open && `scale-0`
+              }`}
+            >
+              Amigos
+            </h1>
+          </a>
         </ul>
       </div>
-      <div>
-        <h1> Home Page</h1>
+      {/*cuerpo de la página*/}
+      <img
+        onClick={() => {
+          window.location.href = "http://localhost:3000/login";
+        }}
+        className="absolute top-0 right-0 m-4"
+        src="http://localhost:3000/carrito.png"
+      />
+
+      <div className="felx flex-col gap-8 place-self-auto m-auto h-96 w-60">
+        <button
+          className="mt-20 w-80 flex h-20 bg-amber-600 hover:bg-amber-800 text-white text-center font-bold px-24 pt-4 border-b-4 border-amber-900 hover:border-black/40 rounded-lg"
+          onClick={() => {
+            window.location.href = "http://localhost:3000/login";
+          }}
+        >
+          BUSCAR PARTIDA
+        </button>
+        <button
+          className="mt-20 w-80 flex h-20 bg-amber-600 hover:bg-amber-800 text-white text-center font-bold px-24 pt-4 border-b-4 border-amber-900 hover:border-black/40 rounded-lg"
+          onClick={() => {
+            window.location.href = "http://localhost:3000/login";
+          }}
+        >
+          CREAR PARTIDA CON AMIGOS
+        </button>
       </div>
     </div>
   );
