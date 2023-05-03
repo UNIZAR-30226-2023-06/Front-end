0aadea4e6776f5f8feeedf9371ca5f63e04567ad
PepeOte <891835@unizar.es>
2023-05-03 21:52:51 +0200

    Cambios en el popUp de fase de construccion
    Añadido la tabla de costes e intentado conectar con backend

--- src/pop-up-FaseCompra.js
+++ src/pop-up-FaseCompra.js
@@ -38,7 +38,7 @@ const PopUpFaseCompra = (props) => {
         >
           <div
             className="relative bg-gray-400 rounded-lg p-4 inline-flex flex-col items-center h-4/5 "
-            style={{ minHeight: "640px", minWidth: "1290px" }}
+            style={{ minHeight: "640px", width:"1000px" }}
           >
             <h1 className="text-3xl mt-2">FASE DE COMPRA Y CONSTRUCCION</h1>
             <img
@@ -51,7 +51,7 @@ const PopUpFaseCompra = (props) => {
 
             <div
               className="absolute inset-y-0 left-0 w-1/4 bg-gray-900 mt-5 ml-7 rounded-lg"
-              style={{ maxHeight: "745px", maxWidth: "200px" }}
+              style={{ maxHeight: "745px", maxWidth: "200px", marginLeft:"20px" }}
             >
               <div
                 className="flex items-center"
@@ -119,94 +119,121 @@ const PopUpFaseCompra = (props) => {
                 <h1 className="text-4xl text-white ml-2">0</h1>
               </div>
             </div>
-            <div class="flex">
-              <div class="flex flex-col items-center"
-              style={{marginBottom:"50px"}}>
-                <div class="rounded-lg overflow-hidden">
+            <div>
+              <div class="flex">
+                <div
+                  class="flex flex-col items-center"
+                  style={{ marginBottom: "50px" }}
+                >
+                  <div class="rounded-lg overflow-hidden">
+                    <img
+                      src="http://localhost:3000/iconos-no-recursos/poblado.png"
+                      alt="Foto 1"
+                      style={{ height: "150px", width: "150px" }}
+                    />
+                  </div>
+                  <div class="mt-10">
+                    <button
+                      class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-4 px-6 border border-gray-400 rounded shadow ml-2"
+                      style={{
+                        fontSize: "20px",
+                        marginLeft: "20px",
+                        width: "125px",
+                      }}
+                    >
+                      Comprar poblado
+                    </button>
+                  </div>
+                </div>
+
+                <div class="rounded-lg overflow-hidden ml-4">
                   <img
-                    src="http://localhost:3000/iconos-no-recursos/poblado.png"
-                    alt="Foto 1"
+                    src="http://localhost:3000/iconos-no-recursos/ciudad.png"
+                    alt="Foto 2"
                     style={{ height: "150px", width: "150px" }}
                   />
-                </div>
-                <div class="mt-10">
-                  <button
-                    class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-4 px-6 border border-gray-400 rounded shadow ml-2"
-                    style={{
-                      fontSize: "20px",
-                      marginLeft: "20px",
-                      width: "125px",
-                    }}
-                  >
-                    Comprar poblado
-                  </button>
+                  <div class="mt-10">
+                    <button
+                      class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-4 px-6 border border-gray-400 rounded shadow ml-2"
+                      style={{
+                        fontSize: "20px",
+                        marginLeft: "20px",
+                        width: "125px",
+                      }}
+                    >
+                      Comprar ciudad
+                    </button>
+                    
+                  </div>
                 </div>
               </div>
-
-              <div class="rounded-lg overflow-hidden ml-4">
-                <img
-                  src="http://localhost:3000/iconos-no-recursos/ciudad.png"
-                  alt="Foto 2"
-                  style={{ height: "150px", width: "150px" }}
+              <div class="flex">
+                <div className="rounded-lg overflow-hidden">
+                  <img
+                    src="http://localhost:3000/iconos-no-recursos/carretera.png"
+                    alt="Foto 3"
+                    style={{ height: "150px", width: "150px" }}
                   />
-                                  <div class="mt-10">
-                  <button
-                    class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-4 px-6 border border-gray-400 rounded shadow ml-2"
+                  <div class="mt-10">
+                    <button
+                      class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-4 px-6 border border-gray-400 rounded shadow ml-2"
+                      style={{
+                        fontSize: "20px",
+                        marginLeft: "20px",
+                        width: "125px",
+                      }}
+                    >
+                      Comprar carretera
+                    </button>
+                  </div>
+                </div>
+                <div className="rounded-lg overflow-hidden">
+                  <img
+                    src="http://localhost:3000/iconos-no-recursos/carta-de-desarrollo.png"
+                    alt="Foto 4"
                     style={{
-                      fontSize: "20px",
-                      marginLeft: "20px",
-                      width: "125px",
+                      height: "150px",
+                      width: "150px",
+                      marginLeft: "40px",
                     }}
-                  >
-                    Comprar ciudad
-                  </button>
+                  />
+                  
+                  <div class="mt-10">
+                    <button
+                      class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-4 px-6 border border-gray-400 rounded shadow ml-2"
+                      style={{
+                        fontSize: "20px",
+                        marginLeft: "20px",
+                        width: "195px",
+                      }}
+                      
+                    >
+                      Comprar cartas de desarrollo
+                    </button>
+                    
+                  </div>
                 </div>
               </div>
+              
             </div>
-            <div class="flex">
-              <div className="rounded-lg overflow-hidden">
-                <img
-                  src="http://localhost:3000/iconos-no-recursos/carretera.png"
-                  alt="Foto 3"
-                  style={{ height: "150px", width: "150px" }}
-                  />
-                                  <div class="mt-10">
-                  <button
-                    class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-4 px-6 border border-gray-400 rounded shadow ml-2"
-                    style={{
-                      fontSize: "20px",
-                      marginLeft: "20px",
-                      width: "125px",
-                    }}
-                  >
-                    Comprar carretera
-                  </button>
-                </div>
-              </div>
-              <div className="rounded-lg overflow-hidden">
-                <img
-                  src="http://localhost:3000/iconos-no-recursos/carta-de-desarrollo.png"
-                  alt="Foto 4"
-                  style={{ height: "150px", width: "150px" ,marginLeft: "40px"}}
-                  />
-                                  <div class="mt-10">
-                  <button
-                    class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-4 px-6 border border-gray-400 rounded shadow ml-2"
-                    style={{
-                      fontSize: "20px",
-                      marginLeft: "20px",
-                      width: "195px",
-                    }}
-                  >
-                    Comprar cartas de desarrollo
-                  </button>
-                </div>
-              </div>
-            </div>
-
             {props.children}
+
+
           </div>
+          <img
+              src="http://localhost:3000/tabla-de-costes.png"
+              alt="Foto 4"
+              style={{
+                height: "600px",
+                width: "500px",
+                right:"200px",
+                top:"200px",
+                marginLeft:"20px"
+                
+              }}
+            />
         </div>
+        
       )}
     </>
   );
